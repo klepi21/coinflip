4e246c7942b17f7d22010129ea6f948e20df2341
klepi21 <kolepidas@gmail.com>
2025-01-19 18:21:55 +0200

    changes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -11,7 +11,7 @@ export const Footer = () => {
       <div className="fixed bottom-0 left-0 right-0 text-center z-40 bg-[#FFA036]/90 backdrop-blur-lg py-2 border-t-2 border-black/30">
         <div className="text-black font-doggie space-y-1">
           <div className="flex justify-center">
-            <img src="https://bod.gg/assets/logo-DMc1kk4n.svg" alt="BOD Logo" className="mb-2 w-12" />
+            <img src="https://bod.gg/assets/logo-DMc1kk4n.svg" alt="BOD Logo" className=" w-12" />
           </div>
           <div>
             Copyright 2025 $BOD |{' '}
