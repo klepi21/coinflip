6be97ab8e0392a370a2ede568aac0d64b108baf5
klepi21 <kolepidas@gmail.com>
2025-01-25 15:48:27 +0200

    changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const mvxConfig = {
   chainId: '1', 
-  apiUrl: 'https://api.multiversx.com',
+  apiUrl: 'https://multiversx-api.beaconx.app/public-mainnet-api',
   gatewayUrl: 'https://multiversx-api.beaconx.app/public-mainnet-gateway',
   explorerUrl: 'https://explorer.multiversx.com',
   walletConnectV2ProjectId: '9b1a9564f91cb659ffe21b73d5c4e2d8'
